b9b02ec55b2795b91f55ee11d38011ca6aaa5bdd
NovakiTamas <novaki.tamas@students.jedlik.eu>
2024-06-16 15:37:20 +0200

    felelok

--- feleles.py
+++ feleles.py
@@ -5,19 +5,19 @@ def main() -> None:
     diakok: list = ["Bence", "Tamás", "Benedek", "Gyula", "Ábel"]
     felelokszama = int(input("Adja meg hány diák felel ma: "))
 
-    if felelokszama > len(diakok):
+    if felelokszama > len(diakok): #tul sok felelő meghatározása
         print("Nincs ennyi diák az osztályban")
     else:
 
         if felelokszama == 1:
 
-            felelo = random.choice(diakok)
+            felelo = random.choice(diakok) #egy diák kiválasztása
             print(f"A mai felelő: {felelo}")
 
         else:
 
-            felelok = random.sample(diakok, felelokszama)
-            felelok_str = ", ".join(felelok)
+            felelok = random.sample(diakok, felelokszama) #több diák kiválasztása
+            felelok_str = ", ".join(felelok) 
             print(f"A mai felelők: {felelok_str}")
 
 if __name__ == "__main__":
